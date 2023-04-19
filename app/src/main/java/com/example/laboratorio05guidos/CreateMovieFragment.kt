c59c9781707c33e5e75a5aba45676af005a53658
AngelGuidos <00137321@uca.edu.sv>
2023-04-19 13:07:01 -0600

    Lab05

---
+++ app/src/main/java/com/example/laboratorio05guidos/CreateMovieFragment.kt
@@ -0,0 +1,27 @@
+package com.example.laboratorio05guidos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class CreateMovieFragment : Fragment() {
+
+    private lateinit var actionSubmit: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        actionSubmit = view.findViewById(R.id.actio_submit)
+    }
+}
